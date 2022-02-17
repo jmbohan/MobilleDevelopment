ae1e036e7de0f943eed313f91420127a91f4e97e
jmbohan <jmbohan@purdue.edu>
2022-02-17 20:15:05 +0000

    added DataItem.java

--- demoCustomListViewTextImage/app/src/main/java/com/example/demoCustomListViewTextImage/MainActivity.java
+++ demoCustomListViewTextImage/app/src/main/java/com/example/demoCustomListViewTextImage/MainActivity.java
@@ -1,13 +1,22 @@
-package com.example.chris.fstest;
+package com.example.customlistviewtextimage;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<DataItem> dataItemList = SampleDataProvider.dataItemList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomAdapter adapter = new CustomAdapter(this, dataItemList);
+        ListView itemListView = findViewById(R.id.my_list);
+        itemListView.setAdapter(adapter);
     }
 }
